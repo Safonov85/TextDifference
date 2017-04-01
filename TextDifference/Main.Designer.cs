86f501ce3b667011806f3a9823ec4a296a2c5745
Safonov85 <safonov85@yahoo.com>
2017-04-01 21:46:13 +0200

    First commit

---
+++ TextDifference/Main.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace TextDifference
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadText1Button = new System.Windows.Forms.Button();
+            this.LoadText2Button = new System.Windows.Forms.Button();
+            this.Text1RichTextBox = new System.Windows.Forms.RichTextBox();
+            this.Text2RichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // LoadText1Button
+            // 
+            this.LoadText1Button.Location = new System.Drawing.Point(160, 47);
+            this.LoadText1Button.Name = "LoadText1Button";
+            this.LoadText1Button.Size = new System.Drawing.Size(83, 35);
+            this.LoadText1Button.TabIndex = 0;
+            this.LoadText1Button.Text = "Load Text 1";
+            this.LoadText1Button.UseVisualStyleBackColor = true;
+            this.LoadText1Button.Click += new System.EventHandler(this.LoadText1Button_Click);
+            // 
+            // LoadText2Button
+            // 
+            this.LoadText2Button.Location = new System.Drawing.Point(566, 47);
+            this.LoadText2Button.Name = "LoadText2Button";
+            this.LoadText2Button.Size = new System.Drawing.Size(84, 35);
+            this.LoadText2Button.TabIndex = 1;
+            this.LoadText2Button.Text = "Load Text 2";
+            this.LoadText2Button.UseVisualStyleBackColor = true;
+            this.LoadText2Button.Click += new System.EventHandler(this.LoadText2Button_Click);
+            // 
+            // Text1RichTextBox
+            // 
+            this.Text1RichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Text1RichTextBox.Location = new System.Drawing.Point(25, 91);
+            this.Text1RichTextBox.Name = "Text1RichTextBox";
+            this.Text1RichTextBox.Size = new System.Drawing.Size(370, 442);
+            this.Text1RichTextBox.TabIndex = 4;
+            this.Text1RichTextBox.Text = "";
+            // 
+            // Text2RichTextBox
+            // 
+            this.Text2RichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Text2RichTextBox.Location = new System.Drawing.Point(417, 91);
+            this.Text2RichTextBox.Name = "Text2RichTextBox";
+            this.Text2RichTextBox.Size = new System.Drawing.Size(386, 442);
+            this.Text2RichTextBox.TabIndex = 5;
+            this.Text2RichTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(826, 559);
+            this.Controls.Add(this.Text2RichTextBox);
+            this.Controls.Add(this.Text1RichTextBox);
+            this.Controls.Add(this.LoadText2Button);
+            this.Controls.Add(this.LoadText1Button);
+            this.MinimumSize = new System.Drawing.Size(842, 598);
+            this.Name = "MainForm";
+            this.Text = "Text Difference";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadText1Button;
+        private System.Windows.Forms.Button LoadText2Button;
+        private System.Windows.Forms.RichTextBox Text1RichTextBox;
+        private System.Windows.Forms.RichTextBox Text2RichTextBox;
+    }
+}
+
---
+++ TextDifference/Main.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextDifference
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Text1RichTextBox.SelectionBackColor = Color.Yellow;
+            Text1RichTextBox.AutoWordSelection = false;
+        }
+
+        private void LoadText1Button_Click(object sender, EventArgs e)
+        {
+            SelectFileFromDialog();
+
+            HighlightText();
+        }
+
+        void SelectFileFromDialog()
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+
+                        using (TextReader reader = new StreamReader(myStream, Encoding.GetEncoding(1252), true)) // Using special encoding to get Swedish åäö letters
+                        {
+                            String text = reader.ReadToEnd();
+                            Text1RichTextBox.Text = text;
+                        }
+
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        void HighlightText()
+        {
+            if (String.IsNullOrEmpty(Text1RichTextBox.Text))
+            {
+                return;
+            }
+            int start = 0;
+            int end = Text1RichTextBox.Text.LastIndexOf("hoppade");
+
+            Text1RichTextBox.SelectAll();
+            Text1RichTextBox.SelectionBackColor = Color.Transparent;
+
+            while (start < end)
+            {
+                Text1RichTextBox.Find("hoppade", start, Text1RichTextBox.TextLength, RichTextBoxFinds.MatchCase);
+
+                Text1RichTextBox.SelectionBackColor = Color.Yellow;
+
+                start = Text1RichTextBox.Text.IndexOf("hoppade", start) + 1;
+            }
+        }
+
+        private void LoadText2Button_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
