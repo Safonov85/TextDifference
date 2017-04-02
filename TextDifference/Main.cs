cb407d1018fe5443ec52657ebaa1fba327da34e2
Safonov85 <safonov85@yahoo.com>
2017-04-02 21:55:05 +0200

    More Code

--- TextDifference/Main.cs
+++ TextDifference/Main.cs
@@ -13,26 +13,35 @@ namespace TextDifference
 {
     public partial class MainForm : Form
     {
+        string firstText, secondText;
+
         public MainForm()
         {
             InitializeComponent();
-            
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
             Text1RichTextBox.SelectionBackColor = Color.Yellow;
             Text1RichTextBox.AutoWordSelection = false;
+            Text1RichTextBox.Text = null;
+            Text2RichTextBox.Text = null;
         }
 
         private void LoadText1Button_Click(object sender, EventArgs e)
         {
-            SelectFileFromDialog();
+            SelectFileFromDialog("1");
 
-            HighlightText();
+            //GetMissingWord();
+            //HighlightText();
         }
 
-        void SelectFileFromDialog()
+        private void LoadText2Button_Click(object sender, EventArgs e)
+        {
+            SelectFileFromDialog("2");
+        }
+
+        void SelectFileFromDialog(string richTextBox)
         {
             Stream myStream = null;
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
@@ -52,10 +61,18 @@ namespace TextDifference
                         using (TextReader reader = new StreamReader(myStream, Encoding.GetEncoding(1252), true)) // Using special encoding to get Swedish åäö letters
                         {
                             String text = reader.ReadToEnd();
-                            Text1RichTextBox.Text = text;
+                            if(richTextBox == "1")
+                            {
+                                Text1RichTextBox.Text = text;
+                                firstText = Text1RichTextBox.Text;
+                            }
+                            else
+                            {
+                                Text2RichTextBox.Text = text;
+                                secondText = Text2RichTextBox.Text;
+                            }
+                            
                         }
-
-
                     }
                 }
                 catch (Exception ex)
@@ -63,6 +80,48 @@ namespace TextDifference
                     MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
                 }
             }
+        }
+
+        void CompareStrings(string text1, string text2)
+        {
+            int result = 0;
+
+            result = string.Compare(text1, text2);
+
+
+        }
+
+        void GetMissingWord()
+        {
+            if (string.IsNullOrEmpty(firstText))
+            {
+                return;
+            }
+            if (string.IsNullOrEmpty(secondText))
+            {
+                return;
+            }
+            List<string> listFirstText = new List<string>();
+            List<string> listSecondText = new List<string>();
+
+            // Add words to First Text list
+            string[] wordsText1 = firstText.Split(' ');
+            foreach (string word in wordsText1)
+            {
+                listFirstText.Add(word);
+            }
+
+            // Add words to Second Text list
+            string[] wordsText2 = secondText.Split(' ');
+            foreach (string word in wordsText2)
+            {
+                listSecondText.Add(word);
+            }
+
+            //foreach(string wurd in listFirstText)
+            //{
+            //    Text1RichTextBox.Text += Environment.NewLine + wurd;
+            //}
         }
 
         void HighlightText()
@@ -77,19 +136,16 @@ namespace TextDifference
             Text1RichTextBox.SelectAll();
             Text1RichTextBox.SelectionBackColor = Color.Transparent;
 
-            while (start < end)
-            {
+            //while (start < end)
+            //{
                 Text1RichTextBox.Find("hoppade", start, Text1RichTextBox.TextLength, RichTextBoxFinds.MatchCase);
 
                 Text1RichTextBox.SelectionBackColor = Color.Yellow;
 
                 start = Text1RichTextBox.Text.IndexOf("hoppade", start) + 1;
-            }
+            //}
         }
 
-        private void LoadText2Button_Click(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
